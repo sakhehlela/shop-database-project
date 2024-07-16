5ae08098839bf98a2331c8049a3585af4729d350
Sakhe Hlela <sakhehlela@gmail.com>
2024-07-16 18:19:50 +0200

    initial commit

--- src/create-tables.sql
+++ src/create-tables.sql
@@ -1,3 +1,9 @@
+DROP TABLE IF EXISTS Orders;
+DROP TABLE IF EXISTS Payments;
+DROP TABLE IF EXISTS Products;
+DROP TABLE IF EXISTS Employees;
+DROP TABLE IF EXISTS Customers;
+
 CREATE TABLE Customers (
     ID SERIAL PRIMARY KEY,
     FirstName VARCHAR(50),
@@ -6,7 +12,7 @@ CREATE TABLE Customers (
     Address VARCHAR(200),
     Phone VARCHAR(20),
     Email VARCHAR(100),
-    City VARCHAR(50),
+    City VARCHAR(20),
     Country VARCHAR(50)
 );
 
@@ -15,29 +21,33 @@ CREATE TABLE Employees (
     FirstName VARCHAR(50),
     LastName VARCHAR(50),
     Email VARCHAR(100),
-    JobTitle VARCHAR(50)
+    JobTitle VARCHAR(20)
 );
 
 CREATE TABLE Products (
     ID SERIAL PRIMARY KEY,
     ProductName VARCHAR(100),
     Description VARCHAR(300),
-    BuyPrice DECIMAL(10, 2)
+    BuyPrice DECIMAL
 );
 
 CREATE TABLE Payments (
     ID SERIAL PRIMARY KEY,
-    CustomerID INT REFERENCES Customers(ID),
+    CustomerID INT,
     PaymentDate TIMESTAMP,
-    Amount DECIMAL(10, 2)
+    Amount DECIMAL,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(ID)
 );
 
 CREATE TABLE Orders (
     ID SERIAL PRIMARY KEY,
-    ProductID INT REFERENCES Products(ID),
-    PaymentID INT REFERENCES Payments(ID),
-    FulfilledByEmployeeID INT REFERENCES Employees(ID),
+    ProductID INT,
+    PaymentID INT,
+    FulfilledByEmployeeID INT,
     DateRequired TIMESTAMP,
     DateShipped TIMESTAMP,
-    Status VARCHAR(20)
+    Status VARCHAR(20),
+    FOREIGN KEY (ProductID) REFERENCES Products(ID),
+    FOREIGN KEY (PaymentID) REFERENCES Payments(ID),
+    FOREIGN KEY (FulfilledByEmployeeID) REFERENCES Employees(ID)
 );
--- src/insert-records.sql
+++ src/insert-records.sql
@@ -1,4 +1,31 @@
 INSERT INTO Customers (FirstName, LastName, Gender, Address, Phone, Email, City, Country)
 VALUES
-('John', 'Hibert', 'Male', '284 Chaucer St', '084789657', 'john@gmail.com', 'Johannesburg', 'South Africa'),
-('Thando', 'Sithole', 'Female', '240 Sect 1', '0794445584', 'thando@gmail.com', 'Cape Town', 'South Africa');
+('John', 'Hibert', 'Male', '284 chaucer st', '084789657', 'john@gmail.com', 'Johannesburg', 'South Africa'),
+('Thando', 'Sithole', 'Female', '240 Sect 1', '0794445584', 'thando@gmail.com', 'Cape Town', 'South Africa'),
+('Leon', 'Glen', 'Male', '81 Everton Rd,Gillits', '0820832830', 'Leon@gmail.com', 'Durban', 'South Africa'),
+('Charl', 'Muller', 'Male', '290A Dorset Ecke', '+44856872553', 'Charl.muller@yahoo.com', 'Berlin', 'Germany'),
+('Julia', 'Stein', 'Female', '2 Wernerring', '+448672445058', 'Js234@yahoo.com', 'Frankfurt', 'Germany');
+
+INSERT INTO Employees (FirstName, LastName, Email, JobTitle)
+VALUES
+('Kani', 'Matthew', 'mat@gmail.com', 'Manager'),
+('Lesly', 'Cronje', 'LesC@gmail.com', 'Clerk'),
+('Gideon', 'Maduku', 'm@gmail.com', 'Accountant');
+
+INSERT INTO Products (ProductName, Description, BuyPrice)
+VALUES
+('Harley Davidson Chopper', 'This replica features a working kickstand, front suspension, gear-shift lever', 150.75),
+('Classic Car', 'Turnable front wheels, steering function', 550.75),
+('Sportscar', 'Turnable front wheels, steering function', 700.60);
+
+INSERT INTO Payments (CustomerID, PaymentDate, Amount)
+VALUES
+(1, '2018-09-01', 150.75),
+(5, '2018-09-03', 150.75),
+(4, '2018-09-03', 700.60);
+
+INSERT INTO Orders (ProductID, PaymentID, FulfilledByEmployeeID, DateRequired, DateShipped, Status)
+VALUES
+(1, 1, 2, '2018-09-05', NULL, 'Not shipped'),
+(1, 2, 2, '2018-09-04', '2018-09-03', 'Shipped'),
+(3, 3, 3, '2018-09-06', NULL, 'Not shipped');
