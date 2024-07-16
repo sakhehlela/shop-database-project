836cdba041af73137503b90e03f9a90aae1c4b12
Sakhe Hlela <sakhehlela@gmail.com>
2024-07-16 17:19:00 +0200

    initial commit

--- src/insert-records.sql
+++ src/insert-records.sql
@@ -1,3 +1,4 @@
-INSERT INTO Customers (FirstName, LastName, Gender, Address, Phone, Email, City, Country) VALUES
+INSERT INTO Customers (FirstName, LastName, Gender, Address, Phone, Email, City, Country)
+VALUES
 ('John', 'Hibert', 'Male', '284 Chaucer St', '084789657', 'john@gmail.com', 'Johannesburg', 'South Africa'),
-('Thando', 'Sithole', 'Female', '240 Sect 1', '0794445584', 'thando@gmail.com', 'Cape Town', 'South Africa'),
+('Thando', 'Sithole', 'Female', '240 Sect 1', '0794445584', 'thando@gmail.com', 'Cape Town', 'South Africa');
