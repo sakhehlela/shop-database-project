6caf1433ec79936815e80f47a1fe32aca9dc68b2
Sakhe Hlela <sakhehlela@gmail.com>
2024-07-25 21:15:55 +0200

    fixed the naming in the insert file

--- src/insert_records.sql
+++ src/insert_records.sql
@@ -2,23 +2,23 @@
 
 INSERT INTO customers (first_name, last_name, gender, address, phone, email, city, country)
 VALUES 
-('john', 'hibert', 'male', '284 chaucer st', '084789657', 'john@gmail.com', 'johannesburg', 'south africa'),
-('thando', 'sithole', 'female', '240 sect 1', '0794445584', 'thando@gmail.com', 'cape town', 'south africa'),
-('leon', 'glen', 'male', '81 everton rd,gillits', '0820832830', 'leon@gmail.com', 'durban', 'south africa'),
-('charl', 'muller', 'male', '290a dorset ecke', '+44856872553', 'charl.muller@yahoo.com', 'berlin', 'germany'),
-('julia', 'stein', 'female', '2 wernerring', '+448672445058', 'js234@yahoo.com', 'frankfurt', 'germany');
+('John', 'Hibert', 'Male', '284 Chaucer st', '084789657', 'john@gmail.com', 'Johannesburg', 'South africa'),
+('Thando', 'Sithole', 'Female', '240 sect 1', '0794445584', 'thando@gmail.com', 'Cape town', 'South africa'),
+('Leon', 'Glen', 'Male', '81 Everton Rd,Gillits', '0820832830', 'leon@gmail.com', 'Durban', 'South africa'),
+('Charl', 'Muller', 'Male', '290a Dorset Ecke', '+44856872553', 'charl.muller@yahoo.com', 'Berlin', 'Germany'),
+('Julia', 'Stein', 'Female', '2 Wernerring', '+448672445058', 'js234@yahoo.com', 'Frankfurt', 'Germany');
 
 INSERT INTO employees (first_name, last_name, email, job_title)
 VALUES 
-('kani', 'matthew', 'mat@gmail.com', 'manager'),
-('lesly', 'cronje', 'lesc@gmail.com', 'clerk'),
-('gideon', 'maduku', 'm@gmail.com', 'accountant');
+('Kani', 'Matthew', 'mat@gmail.com', 'Manager'),
+('Lesly', 'Cronje', 'lesc@gmail.com', 'Clerk'),
+('Gideon', 'Maduku', 'm@gmail.com', 'Accountant');
 
 INSERT INTO products (product_name, description, buy_price)
 VALUES 
-('harley davidson chopper', 'this replica features a working kickstand, front suspension, gear-shift lever', 150.75),
-('classic car', 'turnable front wheels, steering function', 550.75),
-('sportscar', 'turnable front wheels, steering function', 700.60);
+('Harley Davidson Chopper', 'This replica features a working kickstand, front suspension, gear-shift lever', 150.75),
+('Classic Car', 'Turnable front wheels, steering function', 550.75),
+('Sportscar', 'Turnable front wheels, steering function', 700.60);
 
 INSERT INTO payments (customer_id, payment_date, amount)
 VALUES 
